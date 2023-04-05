9c55bffabf53394f1324e3e90b0b24cf458a514e
Senua <chc0089@gmail.com>
2023-04-06 01:49:24 +0200

    fixed error: foundInFavorites variable was undefined cause the search wasnt in favorites

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ const app = Vue.createApp({
         const savedFavorites = JSON.parse(window.localStorage.getItem('favorites'))
 
         //si savedFavorites contiene algo, creamos un nuevo mapa en el que devolvemos la id del usuario como key y todo lo demas como value.
-        if(savedFavorites?.length){
+        if (savedFavorites?.length) {
             //La interrogación indica si existe o no la propiedad y en caso de que no, no arrojará un error sino que retornará un valor nulo.
             const favorites = new Map(savedFavorites.map(favorite => [favorite.login, favorite]))
             //asociamos la constante con la propiedad de la instancia.
@@ -28,55 +28,63 @@ const app = Vue.createApp({
 
     //propiedades computadas
     computed: {
-        isFavorite(){
+        isFavorite() {
             return this.favorites.has(this.result.login)
         },
 
         //función para que únicamente nos devuelva el valor del mapa
-        allFavorites(){
+        allFavorites() {
             return Array.from(this.favorites.values())
         }
     },
 
     methods: {
-        async doSearch(){
+        async doSearch() {
             this.result = this.error = null
 
             //Esto lo hacemos para que no esté constantemente haciendo la misma petición http cuando nos encontramos como favoritos y nos volvemos a buscar.
             const foundInFavorites = this.favorites.get(this.search)
 
             const shouldRequestAgain = (() => {
-                if(!!foundInFavorites){
-                    const { lastRequestTime } = foundInFavorites
-                    const now = Date.now()
-                    return (now - lastRequestTime) > requestMaxTimeMs
+                if (!!foundInFavorites) {
+                    const { lastRequest } = foundInFavorites;
+                    return (
+                        new Date().getTime() - new Date(lastRequest).getTime() >
+                        requestMaxTimeMs
+                    );
                 }
-                return false
-            })() //IIFE
+                return false;
+            })(); // IIFE
 
             //la doble interrogación fuerza a la constante a ser evaluada como boolean, un casteo a boolean.
-            if(!!foundInFavorites && !shouldRequestAgain) {
+            if (!!foundInFavorites && !shouldRequestAgain) {
                 console.log('Found and we use the cached version')
-                return this.result = foundInFavorites
+                return (this.result = foundInFavorites)
             }
-                //si obtenemos un return, salimos de la función y no usamos el try/catch
+            //si obtenemos un return, salimos de la función y no usamos el try/catch
 
+            await this.doRequest()
+            if (foundInFavorites) foundInFavorites.lastRequest = new Date();
+        },
+
+        async doRequest() {
             try {
                 console.log('Not found or cached version is too old')
                 //Reactividad en dos sentidos: podemos modificar el valor de la variable search desde el cuadro de búsqueda o desde la devtools de vue
                 //con fetch solicitamos datos via http (Axios) y sustituye a XMLHttpRequest
-            const response = await fetch(API + this.search)
+                this.result = this.error = null;
+                const response = await fetch(API + this.search)
 
-            if(!response.ok) throw new Error('User Not Found') //el ok lo sacamos de la respuesta, será false si el usuario no existe
-            //console.log(response) de aquí sacamos la propiedad ok en false cuando se produce un error de no encontrado.
-            //la respuesta la tenemos que interpretar en formato json
-            const data = await response.json() 
-            console.log(data)
-            this.result = data
-            foundInFavorites.lastRequestTime = Date.now()
-            }catch(error){
+                if (!response.ok) throw new Error('User Not Found') //el ok lo sacamos de la respuesta, será false si el usuario no existe
+                //console.log(response) de aquí sacamos la propiedad ok en false cuando se produce un error de no encontrado.
+                //la respuesta la tenemos que interpretar en formato json
+                const data = await response.json()
+                console.log(data)
+                this.result = data
+
+            } catch (error) {
                 this.error = error
-            } finally{
+            } finally {
                 this.search = null //limpiamos la caja de búsqueda
             }
         },
@@ -96,12 +104,17 @@ const app = Vue.createApp({
             this.updateStorage()
         },
 
-        showFavorite(favorite){
+        showFavorite(favorite) {
             this.result = favorite
 
         },
 
-        updateStorage(){
+
+        checkFavorite(id) {
+            return this.result?.login === id
+        },
+
+        updateStorage() {
             //convertimos la estructura nativa en un string JSON y lo guardamos en el local storage del navegador (persistencia)
             window.localStorage.setItem('favorites', JSON.stringify(this.allFavorites))
         }
