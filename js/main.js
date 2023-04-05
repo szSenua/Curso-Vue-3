9fc573e2cbf6e5bbe1814fba299480c84514c062
Senua <chc0089@gmail.com>
2023-04-05 18:46:51 +0200

    Search function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 const API = "https://api.github.com/users/"
 
+//función de prueba buscar en github
+async function doSearch(){
+    const response = await fetch(API + 'szSenua')
+    //la respuesta la tenemos que interpretar en formato json
+    const data = await response.json() 
+    console.log(data)
+}
+
 const app = Vue.createApp({
     data() {
         return {
