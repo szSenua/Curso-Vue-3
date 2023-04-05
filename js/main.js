25c113fd6dc76096ef92663e03de88aee71701e2
Senua <chc0089@gmail.com>
2023-04-05 18:59:59 +0200

    Methods and Events

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 const API = "https://api.github.com/users/"
 
-//función de prueba buscar en github
-async function doSearch(){
-    const response = await fetch(API + 'szSenua')
-    //la respuesta la tenemos que interpretar en formato json
-    const data = await response.json() 
-    console.log(data)
-}
 
+
+//instancia de Vue
 const app = Vue.createApp({
     data() {
         return {
             message: 'Hello Vue',
+        }
+    },
+
+    methods: {
+        async doSearch(){
+            const response = await fetch(API + 'szSenua')
+            //la respuesta la tenemos que interpretar en formato json
+            const data = await response.json() 
+            console.log(data)
         }
     }
 })
